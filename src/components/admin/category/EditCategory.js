bbdd1d5f6c3ed5fbc74bfa116ae14b9c6b4f53fa
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-02 00:02:53 +0600

    working with category edit and update

--- src/components/admin/category/EditCategory.js
+++ src/components/admin/category/EditCategory.js
@@ -1,74 +1,68 @@
 import axios from 'axios';
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom';
 import swal from 'sweetalert';
 
-const Category = () => {
+const EditCategory = (props) => {
 
-    const [categoryInput, setCategory] = useState({
-        slug: '',
-        name: '',
-        description: '',
-        status: '',
-        meta_title: '',
-        meta_keyword: '',
-        meta_description: '',
-        error_list: [],
-    });
+    const history = useHistory();
+    const [categoryInput, setCategory] = useState([]);
+    const [error, setError] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const category_id = props.match.params.id;
+        axios.get(`/api/edit_category/${category_id}`).then(response => {
+            if(response.data.status === 200){
+                setCategory(response.data.category);
+            }
+            else if(response.data.status === 404){
+                swal("Error", response.data.message, "error");
+                history.pushState("/admin/view_category");
+            }
+            setLoading(false);
+        });
+    }, [props.match.params.id]);
 
     const handleInput = (event) => {
         event.persist();
-        setCategory({...categoryInput, [event.target.name]: event.target.value});
+        setCategory({...categoryInput, [event.target.name] : event.target.value});
     }
 
-    const submitCategory = (event) => {
+    const updateCategory = (event) => {
         event.preventDefault();
-
-        const data = ({
-            slug: categoryInput.slug,
-            name: categoryInput.name, 
-            description: categoryInput.description,
-            status: categoryInput.status,
-            meta_title: categoryInput.meta_title,
-            meta_keyword: categoryInput.meta_keyword,
-            meta_description: categoryInput.meta_description,
-        });
-        axios.post(`/api/store_category`, data).then(response => {
+        const category_id = props.match.params.id;
+        const data = categoryInput;
+        axios.put(`/api/update_category/${category_id}`, data).then(response => {
             if(response.data.status === 200){
                 swal("Success", response.data.message, "success");
-                document.getElementById('CATEGORY_FORM').reset();
+                setError([]);
             }
-            else if(response.data.status === 400){
-                setCategory({...categoryInput, error_list: response.data.errors});
+            else if(response.data.status === 422){
+                swal("All fields are mandatory", "", "warning");
+                setError(response.data.errors);
+            }
+            else if(response.data.status === 404){
+                swal("Error", response.data.message, "error");
+                history.push("/admin/view_category");
             }
         });
     }
 
-    var display_errors = [];
-    if(categoryInput.error_list){
-        display_errors = [
-            categoryInput.error_list.slug,
-            categoryInput.error_list.name,
-            categoryInput.error_list.meta_title,
-        ];
+    if(loading){
+        return <h1 className="text-center">Loading Edit Category...</h1>
     }
 
   return (
-    <div className="container-fluid px-4">
-    <h1 className="mt-4">Create New Category</h1>
-    {
-        display_errors.map( (item, index) => {
-            return(<p className="text-danger mb-1" key={index}>{item}</p>)
-        })
-    }
-   <div className="card mt-4">
+    <div className="container px-4">
+        <div className="card mt-4">
      <div className="card-header">
-       <h4>Add Category
-       <Link to="/admin/view_category" className="btn btn-primary btn-sm float-end">View Category</Link>
+       <h4>Edit Category
+       <Link to="/admin/view_category" className="btn btn-primary btn-sm float-end">Back</Link>
        </h4>
      </div>
      <div className="card-body">
-     <form onSubmit={submitCategory} id="CATEGORY_FORM">
+     <form onSubmit={updateCategory}>
     <ul className="nav nav-tabs" id="myTab" role="tablist">
   <li className="nav-item" role="presentation">
     <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
@@ -83,10 +77,12 @@ const Category = () => {
   <div className="form-group mb-3">
   <label>Slug</label>
   <input type="text" name="slug" onChange={handleInput} value={categoryInput.slug} className="form-control" />
+  <small className="text-danger">{error.slug}</small>
   </div>
   <div className="form-group mb-3">
   <label>Name</label>
   <input type="text" name="name" onChange={handleInput} value={categoryInput.name} className="form-control" />
+  <small className="text-danger">{error.name}</small>
   </div>
   <div className="form-group mb-3">
   <label>Description</label>
@@ -103,6 +99,7 @@ const Category = () => {
   <div className="form-group mb-3">
   <label>Meta Title</label>
   <input type="text" name="meta_title" onChange={handleInput} value={categoryInput.meta_title} className="form-control" />
+  <small className="text-danger">{error.meta_title}</small>
   </div>
   <div className="form-group mb-3">
   <label>Meta Keywords</label>
@@ -116,7 +113,7 @@ const Category = () => {
   </div>
 </div>
 
-<button type="submit" className="btn btn-primary px-4 mt-2 float-end">Submit</button>
+<button type="submit" className="btn btn-primary px-4 mt-2 float-end">Update</button>
 </form>
      </div>
    </div>
@@ -124,4 +121,4 @@ const Category = () => {
   )
 }
 
-export default Category;
+export default EditCategory;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 import Category from "../components/admin/category/Category";
+import EditCategory from "../components/admin/category/EditCategory";
 import ViewCategory from "../components/admin/category/ViewCategory";
 import Dashboard from "../components/admin/Dashboard";
 import Profile from "../components/admin/Profile";
@@ -8,6 +9,7 @@ const routes = [
     {path: "/admin/dashboard", exact:true, name:"Dashboard", component:Dashboard},
     {path: "/admin/add_category", exact:true, name:"Category", component:Category},
     {path: "/admin/view_category", exact:true, name:"ViewCategory", component:ViewCategory},
+    {path: "/admin/edit_category/:id", exact:true, name:"EditCategory", component:EditCategory},
     {path: "/admin/profile", exact:true, name:"Profile", component:Profile},
 ];
 
